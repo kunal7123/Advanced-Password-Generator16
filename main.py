4b78e9a4b6e3dfef4ceb3544b99677dc17228547
Kunal Bairagi <kunalbairagi7123@Gmail.com>
2026-06-06 01:59:22 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+import string
+
+print("==== Advanced Password Generator ====")
+
+length = int(input("Password Length: "))
+
+use_upper = input("Include Uppercase? (y/n): ").lower() == 'y'
+use_numbers = input("Include Numbers? (y/n): ").lower() == 'y'
+use_symbols = input("Include Symbols? (y/n): ").lower() == 'y'
+
+characters = string.ascii_lowercase
+
+if use_upper:
+    characters += string.ascii_uppercase
+
+if use_numbers:
+    characters += string.digits
+
+if use_symbols:
+    characters += string.punctuation
+
+password = ''.join(random.choice(characters) for _ in range(length))
+
+print("\nGenerated Password:")
+print(password)
+
+# Strength Checker
+strength = "Weak"
+
+if length >= 8:
+    strength = "Medium"
+
+if length >= 12 and use_upper and use_numbers and use_symbols:
+    strength = "Strong"
+
+print("Password Strength:", strength)
+
+# Save Password
+save = input("\nSave password to file? (y/n): ")
+
+if save.lower() == 'y':
+    with open("passwords.txt", "a") as file:
+        file.write(password + "\n")
+    print("Password saved successfully!")
+    
